8b651f2e8a52a0cb9f666245cc7fa3a6087d44f5
ahmedcharfeddine013 <ahmedcharfeddine75@gmail.com>
2024-08-17 13:57:30 +0100

    hero section done

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,8 +6,8 @@ import { Button, ButtonGroup } from "@nextui-org/button";
 const Hero = () => {
   return (
     <div className="flex items-center justify-center w-full">
-      <section className="w-full md:block hidden nav-height mx-3 relative">
-        <div className="h-5/6 w-full flex-col flex-center">
+      <section className="w-full md:block hidden nav-height mx-3">
+        <div className="h-5/6 w-full flex-col flex items-center justify-center ">
           <div className="md:w-10/12 w-9/12 rounded-3xl overflow-hidden">
             <video autoPlay loop muted playsInline>
               <source src="/assets/videos/heroVideo.mp4" type="video/mp4" />
@@ -15,13 +15,19 @@ const Hero = () => {
           </div>
         </div>
       </section>
-      <section className="md:hidden h-[85vh] bg-hero-image">
-        <div className="absolute z-50 top-52 left-6 bg-white/30 ">
-          <h1 className="text-6xl text-black font-bold">
-            Discover our luxuary
+      <section className="md:hidden min-h-[85vh] flex items-center justify-center">
+        <div className="flex items-center justify-center z-20 w-full h-full">
+          <h1 className="text-5xl text-white font-bold border mx-10 text-center p-10 bg-gray-200/10 shadow-lg">
+            GET SHOOZY
           </h1>
-          <Button></Button>
         </div>
+        <Image
+          src={"/assets/images/hero.jpg"}
+          alt="hero"
+          objectFit="cover"
+          layout="fill"
+          className="h-full w-full absolute top-0 left-0 -z-20 "
+        />
       </section>
     </div>
   );
